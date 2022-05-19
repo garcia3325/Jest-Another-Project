ae9673fa13cf2550be245a3b1ecc3e113eeb004f
Rene Garcia Jr <renegarciajr.@Renes-MacBook-Air.local>
2022-05-19 14:20:07 -0400

    second commit

---  lib/random.js
+++  lib/random.js
@@ -7,3 +7,17 @@ module.exports = function(val1, val2) {
       return false;
     }
   };
+
+  const randomNumber = require('../lib/random.js');
+
+test('gets random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
+});
+
+const randomNumber = require('../lib/random.js');
+
+test('gets random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
+});
